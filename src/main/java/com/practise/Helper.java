d20ede23e96e414549cfd68ad0e9d0a199fd4b02
Sergei Sandalov <sergei.sandalov@unidata-platform.ru>
2020-04-12 13:31:18 +0300

    Добавил метод bar()

--- src/main/java/com/practise/Helper.java
+++ src/main/java/com/practise/Helper.java
@@ -4,4 +4,8 @@ public class Helper {
     public void foo() {
         System.out.println("For help purpose");
     }
+
+    public void bar() {
+        System.out.println("Another useful method");
+    }
 }

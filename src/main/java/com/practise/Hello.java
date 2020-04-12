e81b891f693de29769425702c3010f33832e3dd5
Sergei Sandalov <sergei.sandalov@unidata-platform.ru>
2020-04-12 12:50:55 +0300

    Поправил строчку печати

--- src/main/java/com/practise/Hello.java
+++ src/main/java/com/practise/Hello.java
@@ -2,6 +2,6 @@ package com.practise;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("com.practise.Hello, world!");
+        System.out.println("Hello, world!");
     }
 }

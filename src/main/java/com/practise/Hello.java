e8735791db62c176351151770998872596e73e3c
Sergei Sandalov <sergei.sandalov@unidata-platform.ru>
2020-04-12 13:30:10 +0300

    Разработка нового функционала. Добавил Helper

--- src/main/java/com/practise/Hello.java
+++ src/main/java/com/practise/Hello.java
@@ -2,6 +2,6 @@ package com.practise;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, world! Change in dev branch");
+        System.out.println("Hello, world! Start developing in dev branch");
     }
 }
---
+++ src/main/java/com/practise/Helper.java
@@ -0,0 +1,7 @@
+package com.practise;
+
+public class Helper {
+    public void foo() {
+        System.out.println("For help purpose");
+    }
+}
